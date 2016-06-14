a672931c4748a56f7f2d015b861b6b055485129e
kono94 <jan.loewenstrom.com>
2016-06-14 15:33:02 +0200

    first commit

---
+++ app/routes.js
@@ -0,0 +1,46 @@
+// app/routes.js
+module.exports = function(app, passport) {
+
+
+
+ // =====================================
+    // LOGIN ===============================
+    // =====================================
+    // show the login form
+    app.get('/', function(req, res) {
+
+        // render the page and pass in any flash data if it exists
+        res.render('login.ejs', { message: req.flash('loginMessage') }); 
+    });
+
+   
+    // =====================================
+    // GAME  =====================
+    // =====================================
+    // we will want this protected so you have to be logged in to visit
+    // we will use route middleware to verify this (the isLoggedIn function)
+    app.get('/game', isLoggedIn, function(req, res) {
+        res.render('game.ejs', {
+            user : req.user // get the user out of session and pass to template
+        });
+    });
+
+    // =====================================
+    // LOGOUT ==============================
+    // =====================================
+    app.get('/logout', function(req, res) {
+        req.logout();
+        res.redirect('/');
+    });
+};
+
+// route middleware to make sure a user is logged in
+function isLoggedIn(req, res, next) {
+
+    // if user is authenticated in the session, carry on 
+    if (req.isAuthenticated())
+        return next();
+
+    // if they aren't redirect them to the home page
+    res.redirect('/');
+}
