0cc8d9832d12f22b74fb5683fe9f7928492846b9
jan <jan.loewenstrom@web.de>
2018-02-17 15:33:49 +0100

    mer

--- server.js
+++ server.js
@@ -4,9 +4,10 @@
 // get all the tools we needvar express  = require('express');
 var express  = require('express');
 var app      = express();
+var fs = require('fs');
 var port     = process.env.PORT || 3000;
 var mysql    = require('mysql');
-var server = require('http').createServer(app);
+var server = require('https').createServer(app);
 var io = require('socket.io')(server);
 var ejs = require('ejs');
 var _ = require('underscore')._;
